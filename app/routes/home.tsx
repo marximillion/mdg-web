94a4c13e729f7f129e14d0567a3471b494578781
marximillion <marximillion@gmail.com>
2025-11-19 01:52:46 -0700

    [Baymarx] - WIP - Update home page - non functional fields - need styling clean up

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import logo from "../assets/logo-lab3-white.png";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "LAB<3 Development" },
+    { name: "description", content: "Development Environment" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const title = "Lab<3 Development";
+  return (
+    <div className="screenContainer">
+      <div className="bannerContainer" />
+      <div className="formContainer">
+        <form onSubmit={() => {}}>
+          <h1>{"First Name"}</h1>
+          <input className="field" type="text" name="query" placeholder="Enter first name" />
+          <h1>{"Last Name"}</h1>
+          <input className="field" type="text" name="query" placeholder="Enter last name"/>
+        </form>
+        <button
+          type="submit"
+          className="button"
+        >
+          {"Submit"}
+        </button>
+      </div>
+    </div>
+  );
 }
